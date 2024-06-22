21c66b515054e5da6086f5245907a901aab21c35
Allen Conlon <software@conlon.dev>
2024-06-22 18:54:57 -0400

    Updated settings

--- hosts/epona/disk-configuration.nix
+++ hosts/epona/disk-configuration.nix
@@ -28,30 +28,41 @@
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ]; # Override existing partition
+                mountpoint = "/media/btrfsroots/root";
                 # Subvolumes must set a mountpoint in order to be mounted,
                 # unless their parent is mounted
                 subvolumes = {
                   # Subvolume name is different from mountpoint
-                  "/rootfs" = {
+                  "@rootfs" = {
                     mountpoint = "/";
                   };
                   # Subvolume name is the same as the mountpoint
-                  "/home" = {
+                  "@home" = {
                     mountOptions = [ "compress=zstd" ];
                     mountpoint = "/home";
                   };
-                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                  "/home/custodian" = { };
                   # Parent is not mounted so the mountpoint must be set
-                  "/nix" = {
+                  "@nix" = {
                     mountOptions = [
-                      "compress=zstd"
                       "noatime"
+                      "compress-force=zstd:1"
+                      "ssd"
+                      "space_cache=v2"
+                      "commit=15"
                     ];
                     mountpoint = "/nix";
                   };
+                  # "@log" = {
+                  #   mountOptions = [
+                  #     "noatime"
+                  #     "compress-force=zstd:1"
+                  #     "ssd"
+                  #     "space_cache=v2"
+                  #     "commit=15"
+                  #   ];
+                  #   mountpoint = "/var/log";
+                  # };
                 };
-                mountpoint = "/partition-root";
               };
             };
           };
