60c49bdc2d04abcffd2439d1376ef47395020ce6
Allen Conlon <software@conlon.dev>
2024-10-09 21:06:52 -0400

    Updated config

--- modules/default.nix
+++ modules/default.nix
@@ -60,18 +60,6 @@
       max-free = ${toString (1024 * 1024 * 1024 * 4)}
     '';
   };
-  nixpkgs.overlays = [
-    (_final: prev: {
-      nh = prev.nh.overrideAttrs (oldAttrs: {
-        patches = oldAttrs.patches ++ [
-          (prev.fetchpatch2 {
-            url = "https://github.com/PaulGrandperrin/nh-root/commit/bea0ce26e4b1a260e285164c49456d70d346c924.patch"; # don't bail when non-root
-            hash = "sha256-w8/nfMkk/CeOaLW2XIUvKs7//bGm11Cj6ifyTYzlqjo=";
-          })
-        ];
-      });
-    })
-  ];
   programs.bash.enableCompletion = true;
   services.openssh = {
     enable = true;
--- modules/matchbox/default.nix
+++ modules/matchbox/default.nix
@@ -36,25 +36,25 @@ in
     ca-crt = {
       owner = "${config.users.users.matchbox.name}";
       group = "${config.users.groups.matchbox.name}";
-      sopsFile = "${path}/secrets/dhcp/matchbox.yml";
+      sopsFile = ../../secrets/dhcp/matchbox.yml;
       mode = "0600";
     };
     tls-crt = {
       owner = "${config.users.users.matchbox.name}";
       group = "${config.users.groups.matchbox.name}";
-      sopsFile = "${path}/secrets/dhcp/matchbox.yml";
+      sopsFile = ../../secrets/dhcp/matchbox.yml;
       mode = "0600";
     };
     tls-key = {
       owner = "${config.users.users.matchbox.name}";
       group = "${config.users.groups.matchbox.name}";
-      sopsFile = "${path}/secrets/dhcp/matchbox.yml";
+      sopsFile = ../../secrets/dhcp/matchbox.yml;
       mode = "0600";
     };
     env = {
       owner = "${config.users.users.matchbox.name}";
       group = "${config.users.groups.matchbox.name}";
-      sopsFile = "${path}/secrets/dhcp/matchbox.yml";
+      sopsFile = ../../secrets/dhcp/matchbox.yml;
       mode = "0600";
     };
   };
