5e7591e59c906c0adb896b4d2b548443e40f8104
Allen Conlon <software@conlon.dev>
2025-01-03 20:09:14 -0500

    Updated formats

--- treefmt.nix
+++ treefmt.nix
@@ -5,27 +5,6 @@
 
   programs.deadnix.enable = true;
 
-  programs.dprint = {
-    enable = true;
-    settings = {
-      includes = [
-        "**/*.json"
-        "**/*.md"
-        "**/*.toml"
-      ];
-      excludes = [ "flake.lock" ];
-      plugins =
-        let
-          dprintWasmPluginUrl = n: v: "https://plugins.dprint.dev/${n}-${v}.wasm";
-        in
-        [
-          (dprintWasmPluginUrl "json" "0.19.0")
-          (dprintWasmPluginUrl "markdown" "0.17.0")
-          (dprintWasmPluginUrl "toml" "0.6.2")
-        ];
-    };
-  };
-
   programs.keep-sorted.enable = true;
 
   programs.nixfmt = {
