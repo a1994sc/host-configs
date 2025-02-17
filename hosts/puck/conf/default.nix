56bd9e2120dd7eb0053c473b899685c2d8442fa5
Allen Conlon <software@conlon.dev>
2025-02-16 19:16:47 -0500

    updated puck

--- hosts/puck/conf/default.nix
+++ hosts/puck/conf/default.nix
@@ -34,6 +34,8 @@
   nix.settings.substituters = [
     "https:10.3.10.5:443?priority=10"
     "https:10.3.10.6:443?priority=10"
+    "https:10.3.10.5:443/a1994sc?priority=10"
+    "https:10.3.10.6:443/a1994sc?priority=10"
   ];
   nix.settings.trusted-public-keys = [
     "a1994sc.cachix.org-1:xZdr1tcv+XGctmkGsYw3nXjO1LOpluCv4RDWTqJRczI="
