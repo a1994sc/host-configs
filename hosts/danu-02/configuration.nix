f5372d8ecb62b65da0aa199a9b97ca983106822c
Allen Conlon <software@conlon.dev>
2025-03-03 19:44:07 -0500

    chore: update alts

--- hosts/danu-02/configuration.nix
+++ hosts/danu-02/configuration.nix
@@ -29,18 +29,24 @@ in
         ];
         ssl.enable = true;
         alts = {
+          # keep-sorted start block=yes
           "ascii" = {
             url = "https://a1994sc.cachix.org";
             key = "a1994sc.cachix.org-1:xZdr1tcv+XGctmkGsYw3nXjO1LOpluCv4RDWTqJRczI=";
           };
-          "terra" = {
-            url = "https://nixpkgs-terraform.cachix.org";
-            key = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+          "community" = {
+            url = "https://nix-community.cachix.org";
+            key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
           };
           "numtide" = {
             url = "https://numtide.cachix.org";
             key = "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=";
           };
+          "terra" = {
+            url = "https://nixpkgs-terraform.cachix.org";
+            key = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+          };
+          # keep-sorted end
         };
       };
       matchbox = {
