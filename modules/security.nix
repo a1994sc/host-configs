256cc8b3be80034fbd53d6ff806d4dcd7f3bca9f
Allen Conlon <software@conlon.dev>
2025-03-05 20:29:56 -0500

    updated

--- modules/security.nix
+++ modules/security.nix
@@ -15,34 +15,25 @@ in
   config = lib.mkIf cfg.enable {
     environment.etc = {
       "login.defs".text = pkgs.lib.mkForce ''
+        DEFAULT_HOME yes
         ENCRYPT_METHOD SHA512
+
+        SYS_UID_MIN 400
+        SYS_UID_MAX 999
+        UID_MIN 1000
+        UID_MAX 29999
+
+        SYS_GID_MIN 400
+        SYS_GID_MAX 999
+        GID_MIN 1000
+        GID_MAX 29999
+
+        TTYGROUP tty
+        TTYPERM 0620
+
+        # Ensure privacy for newly created home directories.
+        UMASK 077
       '';
-      "login.defs".source = lib.mkForce (
-        pkgs.writeText "login.defs" ''
-          DEFAULT_HOME yes
-
-          SYS_UID_MIN 400
-          SYS_UID_MAX 999
-          UID_MIN 1000
-          UID_MAX 29999
-
-          SYS_GID_MIN 400
-          SYS_GID_MAX 999
-          GID_MIN 1000
-          GID_MAX 29999
-
-          TTYGROUP tty
-          TTYPERM 0620
-
-          # Ensure privacy for newly created home directories.
-          UMASK 077
-
-          # Uncomment this and install chfn SUID to allow nonroot
-          # users to change their account GECOS information.
-          # This should be made configurable.
-          #CHFN_RESTRICT frwh
-        ''
-      );
     };
     services.openssh.settings.Ciphers = [
       "aes256-ctr"
