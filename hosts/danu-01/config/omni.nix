e7c42aa91b160ad63695ce0c1628c730263b932d
Allen Conlon <software@conlon.dev>
2025-03-22 18:36:39 -0400

    feat: updated

--- hosts/danu-01/config/omni.nix
+++ hosts/danu-01/config/omni.nix
@@ -1,8 +1,15 @@
 { config, pkgs, ... }:
-
+let
+  color = {
+    magenta = "35";
+    cyan = "36";
+    white = "97";
+  };
+  font.reset = "0";
+  format = font: color: "${font};${color}";
+  escape = input: "\\[\\e[${input};11m\\]";
+in
 {
-  ## Create a user "unifi" and a group for it
-
   users.groups.omni = {
     name = "omni";
     gid = 917; # I setup UID/GID manually since I refer to those later
@@ -16,6 +23,16 @@
     description = "Omni Controller";
     extraGroups = [ "podman" ];
     shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [
+      # keep-sorted start
+      "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACa5MIyu4mLLLc0D5Y0eOWV1JnvvSo68pDJAh4SyC1WyMVK1eOIlpyDlfFNu7wev8fPELJEwbT+pCsjH2FVU8qRNAH17nW1EBn9xWOX7rEnpxOp6X485+jeA0t/a2jB6e7Bcn86Xwa1tPEbIKS6eo530KMLagaCFpl9arv1SGWeh6/YAw== aconlon@puck.adrp.xyz"
+      "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAF/R3bjaZYUB6cJe7jcexHc+n+zk+F+39SH55nHWk1uqX5h+/YSkDlDPl42QfVVcV/kyX21yv3zUO3zl6h+OsDltgH9+VggOJSvrYYWLx5vb9H3gH6y3yfc2V8Eyg6v4svSE2z6SbRmQw/bLmCcCU+C+oC74du/a/VJocT4ib706LMG2A== aconlon@omga.ardp.xyz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+ijO8h19n6GB9am9cek91WjLvpn80w3Y5XthK3Tpo/ jump@adrp.xyz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILTN+22xUz/NIZ/+E3B7bSQAl1Opxg0N7jIVGlAxTJw2 git@conlon.dev"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpoTjm581SSJi51VuyDXkGj+JThQOavxicFgK1Z/YlN pihole@adrp.xyz"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPEVDFj/DsBQNjAoid6lbcJhWWyx5Gq6VzSJGKvK+bR6 pixel7@adrp.xyz"
+      # keep-sorted end
+    ];
   };
 
   nix.settings.allowed-users = [ "omni" ];
@@ -28,7 +45,25 @@
   home-manager.users.omni =
     { pkgs, ... }:
     {
-      programs.bash.enable = true;
+      manual.manpages.enable = false;
+      nixpkgs.config.allowUnfree = true;
+      news.display = "silent";
+      xdg.enable = true;
+
+      programs.bash = {
+        enable = true;
+        enableCompletion = true;
+        bashrcExtra = ''
+          PS1="${escape color.magenta}\u${escape color.white}@\h:${escape color.cyan}[\w]: ${escape font.reset}"
+
+          export PS1
+
+          if [ -d ${config.home.homeDirectory}/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
+            . "${config.home.homeDirectory}/.nix-profile/etc/profile.d/hm-session-vars.sh"
+          fi
+        '';
+      };
+
       services.podman = {
         enable = true;
         autoUpdate.enable = true;
@@ -41,9 +76,7 @@
           Requires = [ ];
           After = [ "network-online.target" ];
         };
-        Install = {
-          WantedBy = [ ];
-        };
+        Install.WantedBy = [ ];
         Service = {
           Type = "forking";
           ExecStartPre = [
