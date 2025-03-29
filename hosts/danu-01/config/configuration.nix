16e96c290b88b2ccc651d495e27158d0b650fdf1
Allen Conlon <software@conlon.dev>
2025-03-29 19:14:46 -0400

    chore: updated logic

--- hosts/danu-01/config/configuration.nix
+++ hosts/danu-01/config/configuration.nix
@@ -81,6 +81,7 @@ in
   networking.hosts = {
     "10.3.10.5" = [
       "danu-01.adrp.xyz"
+      "keycloak.danu-01.adrp.xyz"
     ] ++ (builtins.map (alt: "${alt}.${danu-01.domain}") (builtins.attrNames danu-01.alts));
     "10.3.10.6" = [
       "danu-02.adrp.xyz"
