2bfa93b2d8e2da3cc19daac454ef595548cadc50
Allen Conlon <software@conlon.dev>
2025-03-30 14:57:20 -0400

    chore: updated

--- hosts/danu-01/config/configuration.nix
+++ hosts/danu-01/config/configuration.nix
@@ -90,6 +90,9 @@ in
     "10.3.10.5" = [
       "danu-01.adrp.xyz"
       "keycloak.danu-01.adrp.xyz"
+      "api.danu-01.adrp.xyz"
+      "omni.danu-01.adrp.xyz"
+      "kube.danu-01.adrp.xyz"
     ] ++ (builtins.map (alt: "${alt}.${danu-01.domain}") (builtins.attrNames danu-01.alts));
     "10.3.10.6" = [
       "danu-02.adrp.xyz"
