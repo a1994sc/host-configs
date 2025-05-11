458fdd4429c901319c1c720465a570903dab74b1
Allen Conlon <software@conlon.dev>
2025-05-11 19:25:21 -0400

    feat: update locale

--- default.nix
+++ default.nix
@@ -79,19 +79,8 @@
   nixpkgs.config.allowUnfree = true;
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      # keep-sorted start
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-      # keep-sorted end
-    };
+    extraLocaleSettings.LC_ALL = "en_US.UTF-8";
+    supportedLocales = [ "all" ];
   };
   environment.systemPackages = with pkgs; [
     # keep-sorted start prefix_order=staging,unstable,
